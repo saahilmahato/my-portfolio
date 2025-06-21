17904168691454794e6eb45849c01aeaaf88fd8e
codesaahil <mahatosaahil4@gmail.com>
2025-06-21 13:24:40 +0545

    feat: add profile picture

--- src/lib/sections/Home.svelte
+++ src/lib/sections/Home.svelte
@@ -7,7 +7,7 @@
 	import Section from '$lib/components/Section.svelte';
 	import Subheadline from '$lib/components/Subheadline.svelte';
 
-	const profileImage = 'src/lib/img/profile.jpeg';
+	const profileImage = 'profile.png';
 	const headlineText = "I'm Saahil Mahato. I sudo the impossible";
 	const subHeadlineText =
 		"Building tomorrow's technology with today's coffee and yesterday's sleep schedule.";
