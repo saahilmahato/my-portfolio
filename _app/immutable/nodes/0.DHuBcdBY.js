091615f645c2279565dc9481bf6b57c1b7c6e0fd
codesaahil <codesaahil@users.noreply.github.com>
2025-06-24 05:02:23 +0000

    deploy: 8178d2b67312dbbfb321029ed5ebfd1844277562

---
+++ _app/immutable/nodes/0.DHuBcdBY.js
@@ -0,0 +1,1 @@
+import{c as n,a}from"../chunks/BSJQeo3K.js";import{z as p}from"../chunks/2jdZuTYT.js";import{s}from"../chunks/CBKJRGT7.js";const i=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=p(e);s(t,()=>r.children),a(o,e)}export{u as component,f as universal};
